531711006634a8f6f31f64c096a9bf8a7048b4cb
SirhcPaul <christopher.dobson14@gmail.com>
2024-11-28 16:13:13 +0000

    task 22 complete

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,5 +1,5 @@
 const endpointsJson = require("../endpoints.json");
-const { selectTopics } = require("../models/topics.model");
+const { selectTopics, addTopic } = require("../models/topics.model");
 
 //TASK 2
 exports.getApi = (req, res) => {
@@ -14,3 +14,13 @@ exports.getTopics = (req, res, next) => {
     })
     .catch(next);
 }
+
+//TASK 22
+exports.postTopic = (req, res, next) => {
+    const newTopic = req.body;
+    addTopic(newTopic)
+    .then((topic) => {
+        res.status(201).send({ topic });
+    })
+    .catch(next);
+};
--- models/topics.model.js
+++ models/topics.model.js
@@ -7,3 +7,24 @@ exports.selectTopics = () => {
         return rows;
     });
 };
+
+//TASK 22
+exports.addTopic = (newTopic) => {
+    const { slug, description } = newTopic;
+    if (!slug || typeof slug !== "string") {
+        return Promise.reject({ status: 400, msg: "Bad request: Invalid or missing slug"});
+    }
+    const validDescription = typeof description === "string" ? description : null;
+    return db.query(`
+        INSERT INTO topics (slug, description)
+        VALUES ($1, $2) RETURNING *;`, [slug, validDescription])
+        .then(({ rows }) => {
+            return rows[0];
+        })
+        .catch((err) => {
+            if (err.code === "23505") {
+                return Promise.reject({ status: 400, msg: "Bad request: Topic already exists" });
+            }
+            throw err;
+        });
+};
